9a607d3a5125d1513fc7bf761f2d5c451a26430c
george-kirby <georgeskirby@btinternet.com>
2019-10-23 13:09:00 +0100

    changed duck and area container width to rem not %

--- src/components/DuckDetails.js
+++ src/components/DuckDetails.js
@@ -53,7 +53,7 @@ const DuckDetails = ({
         <p>Mood: {mood}</p>
         <p className={hunger > 9 ? "red" : "normal"}>Hunger: <span id="hunger-value">{hunger}</span></p>
         <p>Owner: {user.username}</p>
-        <p>Area: {area.name}</p>
+        {/* <p>Area: {area.name}</p> */}
         <img src={displayImage} alt={name} width="50%" />
       </div>
       <div>
